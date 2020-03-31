a8846848e15c0636635eb03ea8f1b14ba3d01f7a
f.lino1934 <f.lino1934@hotmail.com>
2020-03-31 12:31:33 -0300

    Relacionamento entre a classe order e user

---
+++ src/main/java/com/felipe/curso/entities/Order.java
@@ -0,0 +1,95 @@
+package com.felipe.curso.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+//Vai criar uma tabela no BD com os seguintes atributos dessa classe 
+@Table(name = "tb_order")
+//Vai dar o nome da tabela de tb_order
+public class Order implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	//Esta definindo como chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//Auto incrementavel no banco
+	private Long id;
+	private Instant moment;
+	
+	@ManyToOne
+	//esta falando que a associação é de muitos para um
+	@JoinColumn(name = "client_id")
+	//Nome da chave estrangeira que vai ter no banco
+	private User client;// O pedido tem um cliente
+
+	public Order() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Order(Long id, Instant moment, User client) {
+
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/com/felipe/curso/entities/User.java
+++ src/main/java/com/felipe/curso/entities/User.java
@@ -1,11 +1,14 @@
 package com.felipe.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -24,6 +27,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")//e esta falando que na classe order ele esta mapeado com o nome de "client", fazemos esse mapeamento para puxar pelo id do cliente os pedidos
+	//De um para muitos 
+	private List<Order> orders = new ArrayList<>();//um usuario tem varios pedidos
 
 	public User() {
 		// TODO Auto-generated constructor stub
@@ -76,6 +83,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
